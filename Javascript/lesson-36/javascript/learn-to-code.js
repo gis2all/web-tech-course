64549aafe42d3bf530a6aaa1718803c8bd5743e6
chao9441 <cwang@esri.com>
2022-04-13 16:13:46 +0800

    Add lesson-36

---
+++ Javascript/lesson-36/javascript/learn-to-code.js
@@ -0,0 +1,49 @@
+// Javascript comparison operators and conditional operators
+
+// var balance = [50.45, 4000.12, -300.50];
+// var studentNames = ["Timmy", "Janessa", "Arun"];
+// var naughtyList = [];
+// naughtyList.push(studentNames[0]);
+
+// var index = naughtyList.indexOf("Timmy");
+// console.log(naughtyList);
+
+// if (index > -1){
+//     naughtyList.splice(index, 1);
+// }
+
+// console.log(naughtyList);
+
+// =============================================
+
+// var myAccountBalance = 300;
+// var nikeShoes = 799.23;
+// var coupon = 400;
+
+// if(nikeShoes <= myAccountBalance){
+//     myAccountBalance -= nikeShoes;
+//     console.log("We just bought some dope shoes!");
+// }
+// else if(nikeShoes - coupon <= myAccountBalance){
+//     console.log("We just bought some dope shoes with a coupon!");
+//     myAccountBalance -= nikeShoes - coupon;
+//     console.log("Account Balance: " + myAccountBalance);
+// }
+// else {
+//     console.log("We don't have enough money to buy shoes!");
+// }
+
+// =============================================
+
+var val1 = 23;
+var val2 = "23";
+
+// ==  compare string
+// === compare value and type
+
+if (val1 === val2){
+    console.log("The two values are equal!");
+}
+else {
+    console.log("The two values are not equal!");
+}
