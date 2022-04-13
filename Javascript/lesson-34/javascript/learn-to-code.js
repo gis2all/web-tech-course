318871c3fb27292ef0b03050829a5d84513cfbe7
chao9441 <cwang@esri.com>
2022-04-13 15:37:31 +0800

    Add lesson-34

---
+++ Javascript/lesson-34/javascript/learn-to-code.js
@@ -0,0 +1,11 @@
+// Javascript Variables And Strings
+
+var firstName = "John";
+var lastName = "JacobJingleHeimerSmith";
+var dateOfBirth = "010-09-821";
+var age = 23;
+var profileImg = "http://coolpicks.com/johnjacob.jpg";
+
+var loginWelcomeMessage = "Welcome, " + firstName + " " + lastName + ". Happy " + age + "rd birthday!";
+
+console.log(loginWelcomeMessage);
