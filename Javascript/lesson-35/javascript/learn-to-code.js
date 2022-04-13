e9f998d27e212960801840ca82596196e5fea582
chao9441 <cwang@esri.com>
2022-04-13 15:45:37 +0800

    Add lesson-35

---
+++ Javascript/lesson-35/javascript/learn-to-code.js
@@ -0,0 +1,14 @@
+// Javascript Numbers and Operators
+
+var sum = 10 + 15;
+var sub = 15 - 10;
+var mul = 10 * 3;
+var div = 9 / 3;
+var mod = 10 % 3;
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(mod);
+
